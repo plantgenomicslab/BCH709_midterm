226edbcb5a0088f2d16308ee93033d21ea4201fb
Tzu-Yuan Chen <tzuyuanc@unr.edu>
2026-03-20 11:38:29 -0700

    BCH709_midterm

--- gc_calculator.py
+++ gc_calculator.py
@@ -20,13 +20,15 @@ def parse_fasta(filename):
                 header = line[1:]
                 sequences[header] = ''
             else:
-                sequences[header] = line
+                sequences[header] += line
 
     return sequences
 
 def calculate_gc(sequence):
     """Calculate GC content as a percentage"""
-    gc_count = sequence.count('G') + sequence.count('C')
+    
+    seq = sequence.upper()
+    gc_count = seq.count('G') + seq.count('C')
     gc_content = gc_count / len(sequence) * 100
     return gc_content
 
@@ -34,7 +36,7 @@ def filter_by_length(sequences, min_length):
     """Filter sequences shorter than min_length"""
     filtered = {}
     for header, seq in sequences.items():
-        if len(seq) > min_length:
+        if len(seq) >= min_length:
             filtered[header] = seq
     return filtered
 
@@ -44,7 +46,7 @@ def main():
         sys.exit(1)
 
     input_file = sys.argv[1]
-    min_length = sys.argv[2]
+    min_length = int(sys.argv[2])
 
     sequences = parse_fasta(input_file)
     filtered = filter_by_length(sequences, min_length)
@@ -56,4 +58,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-```
